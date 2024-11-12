5f6221241848daf4254f82f9b28e8a752789add9
Santosh Kumar Vishwakarma <102176332+nxtbuild@users.noreply.github.com>
2024-11-12 09:19:32 +0530

    Add files via upload

--- EmploymentAPI/EmploymentVerification.Implementation/Repositories/EmploymentRepository.cs
+++ EmploymentAPI/EmploymentVerification.Implementation/Repositories/EmploymentRepository.cs
@@ -8,7 +8,7 @@ namespace EmploymentVerification.Implementation.Repositories
         private readonly List<EmploymentRecord> _employees = new List<EmploymentRecord>
         {
             new EmploymentRecord { EmployeeId = 1, CompanyName = "Test Company A", VerificationCode = "XYZ-007" },
-            new EmploymentRecord { EmployeeId = 2, CompanyName = "Test Company A", VerificationCode = "XYZ-764" },
+            new EmploymentRecord { EmployeeId = 2, CompanyName = "Test Company B", VerificationCode = "XYZ-764" },
             new EmploymentRecord { EmployeeId = 3, CompanyName = "Test Company C", VerificationCode = "XYZ-432" },
             new EmploymentRecord { EmployeeId = 4, CompanyName = "Test Company D", VerificationCode = "XYZ-890" },
             new EmploymentRecord { EmployeeId = 5, CompanyName = "Test Company E", VerificationCode = "XYZ-901" },
